2880fd0839c082122d500608b6b7514cdf019568
Anatolii <anatolii.polovnikov@gmail.com>
2022-11-23 21:51:18 +0500

    Решил задача 13

--- Homework_2.cs
+++ Homework_2.cs
@@ -23,3 +23,40 @@
 // Console.Write("Input number: ");
 // int number = Convert.ToInt32(Console.ReadLine());
 // SecNum(number);
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного
+// числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+// void ThirdNumber(int number)
+// {
+//    int hundred = number % 100; // отбросить двухзначные
+//    if (hundred == number )
+//    {
+//     Console.WriteLine("Третьей цифры нет ");
+//    }
+//    else // поиск третьего числа
+//    {
+//    int result = ((number / 100) % 10);
+//    Console.WriteLine($"{number} -> {result}");
+//    } 
+// }
+// Console.Write("Input number: ");
+//  int newNumber = Convert.ToInt32(Console.ReadLine());
+//  ThirdNumber(newNumber);
+
+//  Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+void DayOfWeek(int number)
+{
+
+}
