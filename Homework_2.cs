65cfd451fff36188515027e667e0767ac3059b4e
Анатолий Половников <C:\Users\apolo\AppData\Roaming\The Bat!>
2022-11-23 10:56:48 +0500

    Ошибся с именем программы, было 3 стало 2

---
+++ Homework_2.cs
@@ -0,0 +1,25 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// void SecNum (int num) 
+// {
+//     if (num < 100 || num >= 1000) // need three-digit number
+//     {
+//         Console.WriteLine("You make a mistake");
+//     }
+
+//     else
+//     {
+//         int result = (num - (num/100)*100 - (num % 10))/10;
+//         Console.WriteLine($"{num} -> {result}");
+//     }
+// }
+
+// Console.Write("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// SecNum(number);
